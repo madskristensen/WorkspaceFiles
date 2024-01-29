cc62897e545f4bd6d83e67aac4ed54b10b241962
Mads Kristensen <madskristensen@users.noreply.github.com>
2024-01-29 11:27:58 -0800

    Updated ability to add/remove folders [release]

--- src/MEF/WorkspaceRootNode.cs
+++ src/MEF/WorkspaceRootNode.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.IO;
+using System.Linq;
+using EnvDTE;
 using MAB.DotIgnore;
 
 namespace WorkspaceFiles
@@ -11,21 +13,36 @@ namespace WorkspaceFiles
     {
         private readonly ObservableCollection<WorkspaceItemNode> _innerItems = [];
         private bool _disposed = false;
-        private readonly List<IDirectoryProvider> _providers = [new LocalSolutionProvider()];
+        private List<DirectoryInfo> _directories;
 
         public WorkspaceRootNode()
         {
             General.Saved += OnSettingsSaved;
+            AddFolderCommand.AddFolderRequest += OnAddFolderRequested;
+            RemoveFolderCommand.RemoveFolderRequest += OnRemoveFolderRequested;
+        }
 
-            foreach (IDirectoryProvider provider in _providers)
+        private void OnRemoveFolderRequested(object sender, string directory)
+        {
+            if (_directories.Any(d => d.FullName == directory))
             {
-                provider.DirectoryChanged += OnDirectoryChanged;
+                var info = new DirectoryInfo(directory);
+                _directories.RemoveAll(d => d.FullName == directory);
+                BuildAllDirectories();
+                PersistDirectoriesAsync().FireAndForget();
             }
         }
 
-        private void OnDirectoryChanged(object sender, FileSystemEventArgs e)
+        private void OnAddFolderRequested(object sender, string directory)
         {
-            BuildAllProvider();
+            var info = new DirectoryInfo(directory);
+
+            if (!_directories.Contains(info))
+            {
+                _directories.Add(info);
+                BuildAllDirectories();
+                PersistDirectoriesAsync().FireAndForget();
+            }
         }
 
         private void OnSettingsSaved(General general)
@@ -33,7 +50,8 @@ namespace WorkspaceFiles
             ThreadHelper.JoinableTaskFactory.RunAsync(async () =>
             {
                 await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
-                BuildAllProvider();
+                UpdateDirectories();
+                BuildAllDirectories();
             }).FireAndForget();
         }
 
@@ -47,29 +65,107 @@ namespace WorkspaceFiles
             {
                 if (_innerItems.Count == 0)
                 {
-                    BuildAllProvider();
+                    UpdateDirectories();
+                    BuildAllDirectories();
                 }
 
                 return _innerItems;
             }
         }
 
-        private void BuildAllProvider()
+        private void BuildAllDirectories()
         {
             _innerItems.Clear();
 
-            foreach (IDirectoryProvider item in _providers)
+            foreach (DirectoryInfo dir in _directories)
             {
-                foreach (DirectoryInfo dir in item.GetDirectories())
-                {
-                    var ignoreList = GetIgnore(dir.FullName);
-
-                    
-                    _innerItems.Add(new WorkspaceItemNode(this, dir, ignoreList));
-                }
+                IgnoreList ignoreList = GetIgnore(dir.FullName);
+                _innerItems.Add(new WorkspaceItemNode(this, dir, ignoreList));
             }
 
-                    RaisePropertyChanged(nameof(HasItems));
+            RaisePropertyChanged(nameof(HasItems));
+        }
+
+        private void UpdateDirectories()
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+            DTE dte = VS.GetRequiredService<DTE, DTE>();
+
+            _directories = [];
+
+            if (!dte.Solution.Globals.VariableExists["FileExplorer"])
+            {
+                if (General.Instance.Enabled && TryGetRoot(out DirectoryInfo dir))
+                {
+                    _directories.Add(dir);
+                }
+            }
+            else
+            {
+                var dirs = dte.Solution.Globals["FileExplorer"].ToString().Split('|');
+                var slnFolder = Path.GetDirectoryName(dte.Solution.FullName);
+
+                foreach (var dir in dirs)
+                {
+                    try
+                    {
+                        var info = new DirectoryInfo(Path.Combine(slnFolder, dir));
+                        if (info.Exists)
+                        {
+                            _directories.Add(info);
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        ex.LogAsync().FireAndForget();
+                    }
+                }
+            }
+        }
+
+        private async Task PersistDirectoriesAsync()
+        {
+            try
+            {
+                await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
+                var raw = string.Join("|", _directories.Select(d => d.FullName));
+                DTE dte = await VS.GetRequiredServiceAsync<DTE, DTE>();
+                dte.Solution.Globals["FileExplorer"] = raw;
+                dte.Solution.Globals.VariablePersists["FileExplorer"] = true;
+            }
+            catch (Exception ex)
+            {
+                await ex.LogAsync();
+            }
+        }
+
+        private static bool TryGetRoot(out DirectoryInfo solRoot)
+        {
+            solRoot = null;
+            Community.VisualStudio.Toolkit.Solution solution = VS.Solutions.GetCurrentSolution();
+
+            if (string.IsNullOrEmpty(solution?.FullPath))
+            {
+                return false;
+            }
+
+            solRoot = new DirectoryInfo(Path.GetDirectoryName(solution.FullPath));
+            DirectoryInfo currentRoot = new(solRoot.FullName);
+
+            while (currentRoot != null)
+            {
+                var dotGit = Path.Combine(currentRoot.FullName, ".git");
+
+                if (Directory.Exists(dotGit))
+                {
+                    solRoot = currentRoot;
+                    return true;
+                }
+
+                currentRoot = currentRoot.Parent;
+            }
+
+            return true;
         }
 
         private IgnoreList GetIgnore(string root)
@@ -91,13 +187,9 @@ namespace WorkspaceFiles
             if (!_disposed)
             {
                 General.Saved -= OnSettingsSaved;
+                AddFolderCommand.AddFolderRequest -= OnAddFolderRequested;
+                RemoveFolderCommand.RemoveFolderRequest -= OnRemoveFolderRequested;
                 DisposeChildren();
-
-                foreach (IDirectoryProvider provider in _providers)
-                {
-                    provider.DirectoryChanged -= OnDirectoryChanged;
-                    provider.Dispose();
-                }
             }
 
             _disposed = true;
