4af623bced6a098c937e41a33cec9ecd1c84ebde
felipegodias <felipe.g.o.dias@outlook.com>
2024-02-17 21:31:17 +0000

    Added support for new file and new folder.

---
+++ src/Commands/NewFolderCommand.cs
@@ -0,0 +1,36 @@
+﻿using System.IO;
+using Microsoft.VisualStudio.PlatformUI;
+
+namespace WorkspaceFiles
+{
+    [Command(PackageIds.NewFolder)]
+    internal sealed class NewFolderCommand : BaseCommand<NewFolderCommand>
+    {
+        protected override void Execute(object sender, EventArgs e)
+        {
+            var itemPath = WorkspaceItemContextMenuController.CurrentItem.Info.FullName;
+            var result = TextInputDialog.Show(
+                "New Folder",
+                "Enter the name of the new folder. (Note that folders are not automatically added into projects)",
+                "NewFolder",
+                input =>
+                {
+                    // Check if the folder name is valid and does not exist. 
+                    // Note that we use Path.GetInvalidFileNameChars() instead of Path.GetInvalidPathChars()
+                    // so we avoid creating a folder that is a subfolder.
+                    var isValidName = input.IndexOfAny(Path.GetInvalidPathChars()) == -1;
+                    var folderExists = Directory.Exists(Path.Combine(itemPath, input));
+                    return isValidName && !folderExists;
+                },
+                out var folderName
+            );
+
+            // If the user cancels the dialog, return.
+            if (!result) return;
+
+            // Create the new folder.
+            var folderPath = Path.Combine(itemPath, folderName);
+            Directory.CreateDirectory(folderPath);
+        }
+    }
+}
