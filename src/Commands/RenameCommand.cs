b122627fe11cf84fea20d32c634a14e12cc37d76
Alperen Köylü <alperenkoylu@gmail.com>
2024-02-21 01:11:32 +0300

    Implementing folder renaming Added warnings for unsuccessful operations

--- src/Commands/RenameCommand.cs
+++ src/Commands/RenameCommand.cs
@@ -11,23 +11,53 @@ namespace WorkspaceFiles
         {
             var oldItemPath = WorkspaceItemContextMenuController.CurrentItem.Info.FullName;
             var oldItemName = WorkspaceItemContextMenuController.CurrentItem.Info.Name;
+
+            var fileAttributes = File.GetAttributes(oldItemPath);
+            var isDirectory = fileAttributes.HasFlag(FileAttributes.Directory);
+
             var result = TextInputDialog.Show(
                 "Rename File",
                 $"Enter the new name of the file for {oldItemName}.",
                 oldItemName,
                 userInput =>
                 {
+                    var OperationResult = string.Empty;
+
                     if (!userInput.Equals(oldItemName))
                     {
-                        var isValidFileName = userInput.IndexOfAny(Path.GetInvalidFileNameChars()) == -1;
+                        var isValidName = userInput.IndexOfAny(Path.GetInvalidFileNameChars()) == -1;
 
-                        var fileExists = File.Exists(Path.Combine(oldItemPath, userInput));
+                        bool exists;
 
-                        if (isValidFileName && !fileExists)
+                        if (isDirectory)
+                        {
+                            exists = Directory.Exists(Path.Combine(Directory.GetParent(oldItemPath).FullName, userInput));
+                        }
+                        else
+                        {
+                            exists = File.Exists(Path.Combine(oldItemPath, userInput));
+                        }
+
+                        if (isValidName && !exists)
                         {
                             return true;
                         }
+
+                        if (exists)
+                        {
+                            OperationResult = $"{(isDirectory ? "Directory" : "File")} \"{userInput}\" already exists.";
+                        }
+                        else
+                        {
+                            OperationResult = $"Invalid {(isDirectory ? "directory" : "file")} name \"{userInput}\".";
+                        }
                     }
+                    else
+                    {
+                        OperationResult = $"You must enter different name than current \"{userInput}\" to rename {(isDirectory ? "directory" : "file")}.";
+                    }
+
+                    VS.MessageBox.ShowWarning(OperationResult);
 
                     return false;
                 },
@@ -35,11 +65,23 @@ namespace WorkspaceFiles
             );
 
             // If the user cancels the dialog, return.
-            if (!result) return;
+            if (!result)
+            {
+                return;
+            }
 
-            var newItemPath = Path.Combine(Path.GetDirectoryName(oldItemPath), newItemName);
+            if (isDirectory)
+            {
+                var newFolderPath = Path.Combine(Directory.GetParent(oldItemPath).FullName, newItemName);
 
-            File.Move(oldItemPath, newItemPath);
+                Directory.Move(oldItemPath, newFolderPath);
+            }
+            else
+            {
+                var newFilePath = Path.Combine(Path.GetDirectoryName(oldItemPath), newItemName);
+
+                File.Move(oldItemPath, newFilePath);
+            }
         }
     }
 }
